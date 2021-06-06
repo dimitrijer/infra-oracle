a42e3519e9e8745a171895dd05814a4f29ba070a
Dimitrije Radojević <dimitrijer@nordeus.com>
2021-06-06 18:03:31 +0200

    Add cache block volume.

---
+++ volumes.tf
@@ -0,0 +1,16 @@
+resource "oci_core_volume" "cache" {
+  compartment_id      = oci_identity_compartment.always_free.id
+  availability_domain = oci_core_instance.kitsune.availability_domain
+
+  display_name = "Cache"
+
+  size_in_gbs = 200
+}
+
+resource "oci_core_volume_attachment" "cache_kitsune" {
+  attachment_type = "paravirtualized"
+  instance_id = oci_core_instance.kitsune.id
+  volume_id = oci_core_volume.cache.id
+
+  display_name = "Cache"
+}
